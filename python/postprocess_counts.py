a9d816e00026f8536d9243817e2353fa6154b177
Sarah Nadeau <sarahnadeau266@gmail.com>
2024-06-26 10:15:46 +0200

    remove copy from normalization function, was messing things up when there's an adata.raw

--- python/postprocess_counts.py
+++ python/postprocess_counts.py
@@ -132,7 +132,8 @@ def load_data(
     gtf_gene_id_colname="gene_id",
     gtf_gene_name_colname="gene_name",
 ):
-    adata = sc.read_h5ad(adata)
+    if isinstance(adata, str):
+        adata = sc.read_h5ad(adata)
     logger.info(
         f"Loaded adata with {adata.n_obs} observations and {adata.n_vars} variables"
     )
@@ -143,14 +144,14 @@ def load_data(
             metadata, sep=metadata_sep, dtype={metadata_colname_sample: str}
         )
 
-        # Add metadata to count data
-        adata.obs["cell_id"] = adata.obs.index
-        adata.obs = adata.obs.merge(
-            right=metadata,
+        # Add metadata to count data without disturbing the index
+        adata.obs = pd.merge(
+            adata.obs.reset_index(),
+            metadata,
             how="left",
-            left_on="cell_id",
+            left_on="index",
             right_on=metadata_colname_sample,
-        )
+        ).set_index("index")
 
     if gtf:
         logger.info("Loading and merging genome annotations")
@@ -160,41 +161,52 @@ def load_data(
             usecols=gtf_cols,
             result_type="pandas",
         )
-        
-        duplicates = annotations.loc[annotations.duplicated(subset="gene_id", keep=False).values, :]
-        if duplicates.shape[0] > 0:
-            logger.warn(f"Some {gtf_gene_id_colname} values duplicated in GTF file, will keep only one:\n{duplicates}")
+        print(f"Loaded GTF file with {annotations.shape[0]} annotations and columns {annotations.columns.values}")
+
+        duplicate_ids = annotations.loc[annotations.duplicated(subset=gtf_gene_id_colname, keep=False).values, :]
+        if duplicate_ids.shape[0] > 0:
+            logger.warn(f"Some {gtf_gene_id_colname} gene ids duplicated in GTF file, will keep only one:\n{duplicate_ids}")
             annotations = annotations.groupby(gtf_gene_id_colname).head(1)
 
         # Add gene names to count data (Geneid is name of index)
         adata.var = adata.var.merge(
             right=annotations, how="left", left_on="Geneid", right_on=gtf_gene_id_colname
         )
-        # Use gene_id if there's no gene_name in annotations
+        # Use gene_id if there's no gene_name in annotations or gene_name is duplicated
         adata.var[gtf_gene_name_colname].fillna("", inplace=True)
         j = 0
+        seen_names = set()
         for i in range(adata.var.shape[0]):
             gene_name = adata.var.iloc[i][gtf_gene_name_colname]
-            if gene_name == "":
+            if (gene_name == "") or (gene_name in seen_names):
                 j += 1
                 gene_id = adata.var.iloc[i][gtf_gene_id_colname]
                 gene_name_idx = int(np.where(adata.var.columns == gtf_gene_name_colname)[0])
                 adata.var.iat[i, gene_name_idx] = gene_id
+            seen_names.add(gene_name)
 
         # Make variables index gene names
         adata.var.set_index(gtf_gene_name_colname, inplace=True, drop=False)
-        adata.var_names_make_unique()
 
         logger.info(
-            f"{j} out of {i + 1} genes had no {gtf_gene_name_colname} entry, using {gtf_gene_id_colname} instead"
+            f"{j} out of {i + 1} genes had no or duplicated {gtf_gene_name_colname} entry, using {gtf_gene_id_colname} instead"
         )
 
     return adata
 
 
 def filter_cells(
-    adata, pct_mt_threshold=10, pct_in_genes_threshold=50, cells_to_remove=None
+    adata, pct_mt_threshold=10, pct_in_genes_threshold=50, cells_to_remove=None, pct_in_genes_colname=None, min_genes_per_cell=None
 ):
+    # Filter out cells in to-remove list
+    if cells_to_remove:
+        adata.obs["to_keep"] = ~adata.obs_names.isin(cells_to_remove)
+        n_removed = adata.n_obs - sum(adata.obs["to_keep"])
+        adata = adata[adata.obs["to_keep"], :]
+        logger.info(
+            f"{n_removed} cells discarded for being in to-remove list"
+        )
+        
     # Filter out cells with >X% mitochondrial genes
     n_total_cells = adata.shape[0]
     n_filtered_cells = sum(adata.obs.pct_counts_mt > pct_mt_threshold)
@@ -203,7 +215,7 @@ def filter_cells(
         f"{n_filtered_cells} out of {n_total_cells} cells discarded for having > {pct_mt_threshold}% mt genes"
     )
 
-    # Filter out cells with >X% reads mapping intergenic regions
+    # Filter out cells with <X% mapped reads in genes
     if ("n_reads_mapped_genes" in adata.obs.columns) & (
         "n_total_mappings" in adata.obs.columns
     ):
@@ -220,18 +232,29 @@ def filter_cells(
         logger.info(
             f"{n_filtered_cells} out of {n_total_cells} cells discarded for having < {pct_in_genes_threshold}% reads mapping in genes"
         )
-    else:
-        logger.info(
-            f"Not filtering based on percent reads mapping in genes because one of adata obs columns 'n_reads_mapped_genes' and 'n_total_mappings' is missing"
-        )
-
-    # Filter out cells in to-remove list
-    if cells_to_remove:
-        adata.obs["to_remove"] = adata.obs.cell_id.isin(cells_to_remove).values
+    elif pct_in_genes_colname:
+        n_total_cells = adata.shape[0]
+        n_filtered_cells = sum(adata.obs[pct_in_genes_colname] < pct_in_genes_threshold)
+        adata.obs["to_remove"] = list(adata.obs[pct_in_genes_colname] < pct_in_genes_threshold)
         adata = adata[adata.obs.to_remove == False, :]
         logger.info(
-            f"{sum(adata.obs.to_remove)} cells discarded for being in to-remove list"
+            f"{n_filtered_cells} out of {n_total_cells} cells discarded for having < {pct_in_genes_threshold}% reads mapping in genes"
         )
+    else:
+        logger.info(
+            f"Not filtering based on percent reads mapping in genes because one of adata obs columns 'n_reads_mapped_genes' and 'n_total_mappings' is missing and no pct_in_genes_colname supplied"
+        )
+
+    # Filter out cells with <X genes detected
+    if min_genes_per_cell:
+        n_total_cells = adata.shape[0]
+        n_filtered_cells = sum(adata.obs.n_genes_by_counts < min_genes_per_cell)
+        adata = adata[adata.obs.n_genes_by_counts >= min_genes_per_cell, :]
+        logger.info(
+            f"{n_filtered_cells} out of {n_total_cells} cells discarded for having < {min_genes_per_cell} genes detected"
+        )
+    
+    logger.info(f"{adata.n_obs} cells remain after cell-based filtering")
 
     adata.raw = adata
     return adata
@@ -253,6 +276,7 @@ def filter_genes(
         adata.var[gene_colname] = adata.var.index.values
 
     # Filter out mitochondrial and ribosomal genes: these are confounders because they are correlated with bad quality (both are large and leak slower through membrane)
+    n_total_genes = adata.n_vars
     n_removed_rps = sum(adata.var[gene_colname].str.startswith("Rps"))
     n_removed_rpl = sum(adata.var[gene_colname].str.startswith("Rpl"))
     n_removed_mt = sum(adata.var[gene_colname].str.startswith("mt-"))
@@ -260,7 +284,7 @@ def filter_genes(
     adata = adata[:, ~adata.var[gene_colname].str.startswith("Rpl")]
     adata = adata[:, ~adata.var[gene_colname].str.startswith("mt-")]
     logger.info(
-        f"{n_removed_rpl + n_removed_rps + n_removed_mt} genes discarded for being auto-detected mitochondrial or ribosomal"
+        f"{n_removed_rpl + n_removed_rps}, {n_removed_mt} out of {n_total_genes} genes discarded for being auto-detected ribosomal, mitochondrial\n"
     )
 
     # Filter out genes we don't want to affect normalization
@@ -303,14 +327,14 @@ def normalize_counts(adata, norm_sum=100000):
     # But only include non-filtered genes in library size calculation
     logger.info(f"Normalizing counts using gene set with {adata.n_vars} genes")
 
-    adata_norm = adata.copy()
-    library_size = np.sum(adata_norm.X, axis=1)
-    adata_norm.X = np.divide(adata_norm.X, library_size[:, None]) * norm_sum
+    # adata_norm = adata.copy()
+    library_size = np.sum(adata.X, axis=1)
+    adata.X = np.divide(adata.X, library_size[:, None]) * norm_sum
 
-    adata_norm.obs["library_size_used_to_normalize"] = library_size
-    adata_norm.var["normalization_sum"] = norm_sum
+    adata.obs["library_size_used_to_normalize"] = library_size
+    adata.var["normalization_sum"] = norm_sum
 
-    return adata_norm
+    return adata
 
 
 def main():
